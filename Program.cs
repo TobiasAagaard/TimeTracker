1a22b33621f235eb1c3375c68b2dc36598ae9202
TobiasAagaard <tach@trifork.com>
2026-02-27 23:05:02 +0100

    added Time tracker banner

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TimeTracker.UI;
+
+namespace TimeTracker
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+            Menus.ShowBanner();
+        }
+    }
+}
+ 
