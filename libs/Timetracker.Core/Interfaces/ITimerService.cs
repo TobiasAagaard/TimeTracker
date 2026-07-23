145c1d2df499bfa7e6f938c668517465d70b54ce
TobiasAagaard <tach@trifork.com>
2026-07-23 13:44:26 +0200

    init start

--- libs/Timetracker.Core/Interfaces/ITimerService.cs
+++
@@ -1,7 +0,0 @@
-namespace Timetracker.Core.Interfaces;
-public interface ITimerService
-{
-    Task<RunningTimer?> GetRunningTimerAsync();
-    Task StartAsync(string title);
-    Task StopAsync();
-}
