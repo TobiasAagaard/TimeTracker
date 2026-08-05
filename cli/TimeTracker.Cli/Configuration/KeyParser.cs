ce252a6b836db0ae51c2847af7a504e8fb0b78a5
TobiasAagaard <tach@trifork.com>
2026-08-05 16:35:00 +0200

    working on the keyparser

---
+++ cli/TimeTracker.Cli/Configuration/KeyParser.cs
@@ -0,0 +1,10 @@
+using Microsoft.Extensions.Configuration;
+
+namespace TimeTracker.Cli.Configuration;
+
+public sealed class KeyParser
+{
+    public static class KeyParser {
+        public static KeyBindings Parse(KeyBindingsOptions)
+    }
+}
